80a75e46845948ed108461467b5dfdb3ab9fbc3c
mohit812000 <166015993+mohit812000@users.noreply.github.com>
2024-05-02 17:17:22 +0530

    initial commit

---
+++ routers/news.router.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { addnews, deletenews, getnews, getnewses, updateNews } from "../controllers/news.controller";
+import auth from "../middlewares/auth.middleware"
+
+const router = express.Router();
+router.get("/get-newses", getnewses);
+router.post("/add-news",addnews);
+router.get("/get-news/:news_id",getnews);
+router.delete("/delete-news/:news_id",deletenews);
+router.put("/update-news/:news_id",updateNews);
+export default router
